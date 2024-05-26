7898d0896004e35ace9560785ad88230ba76078b
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-26 12:16:17 +0530

    react flow and structure

--- 01_basic_react/src/App.js
+++ 01_basic_react/src/App.js
@@ -1,5 +1,12 @@
+import Chai from "./Chai";
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Chai />
+      <p>Hi</p>
+    </>
+  );
 }
 
 export default App;
---
+++ 01_basic_react/src/Chai.jsx
@@ -0,0 +1,5 @@
+function Chai() {
+  return <p>Jai Shree Ram</p>;
+}
+
+export default Chai;
