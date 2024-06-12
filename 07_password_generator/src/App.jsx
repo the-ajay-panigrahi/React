df53791a22b1414e5516fbe314a83dbb61e83c76
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-12 12:42:01 +0530

    used useEffect hook

--- 07_password_generator/src/App.jsx
+++ 07_password_generator/src/App.jsx
@@ -1,22 +1,28 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [characterAllowed, setCharacterAllowed] = useState(false);
-  const [password, setPassword] = useState("");
+  let [password, setPassword] = useState("");
 
   const passwordGenerator = useCallback(() => {
+    let pass = "";
     let passwordString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) passwordString += "0123456789";
     if (characterAllowed) passwordString += "!@#$%^&*-_+=[]{}~`";
     for (let index = 1; index <= length; index++) {
       let character = Math.floor(Math.random() * passwordString.length) + 1;
-      password += passwordString.charAt(character);
+      pass += passwordString.charAt(character);
     }
-    setPassword(password);
+    setPassword(pass);
   }, [length, numberAllowed, characterAllowed, setPassword]);
 
+  // passwordGenerator();
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
   return (
     <>
       <main className="bg-black w-screen h-screen flex justify-center items-center">
