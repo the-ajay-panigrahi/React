7721bf6f3df79de7c73b0b1b007c5068c4f243a7
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-12 14:32:43 +0530

    copy to clipboard functionality done

--- 07_password_generator/src/App.jsx
+++ 07_password_generator/src/App.jsx
@@ -1,14 +1,21 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
+  const [lowerCaseAllowed, setLowerCaseAllowed] = useState(true);
+  const [upperCaseAllowed, setUpperCaseAllowed] = useState(false);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [characterAllowed, setCharacterAllowed] = useState(false);
-  let [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+
+  // useRef hook
+  const passwordReference = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
-    let passwordString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let passwordString = "";
+    if (lowerCaseAllowed) passwordString += "abcdefghijklmnopqrstuvwxyz";
+    if (upperCaseAllowed) passwordString += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     if (numberAllowed) passwordString += "0123456789";
     if (characterAllowed) passwordString += "!@#$%^&*-_+=[]{}~`";
     for (let index = 1; index <= length; index++) {
@@ -16,41 +23,101 @@ function App() {
       pass += passwordString.charAt(character);
     }
     setPassword(pass);
-  }, [length, numberAllowed, characterAllowed, setPassword]);
+  }, [
+    length,
+    upperCaseAllowed,
+    lowerCaseAllowed,
+    numberAllowed,
+    characterAllowed,
+    setPassword,
+  ]);
 
-  // passwordGenerator();
+  const copyPasswordToClipBoard = useCallback(() => {
+    passwordReference.current?.select();
+    passwordReference.current?.setSelectionRange(0, length);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
 
   useEffect(() => {
     passwordGenerator();
-  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
+  }, [
+    length,
+    upperCaseAllowed,
+    lowerCaseAllowed,
+    numberAllowed,
+    characterAllowed,
+    passwordGenerator,
+  ]);
   return (
     <>
-      <main className="bg-black w-screen h-screen flex justify-center items-center">
-        <section className="bg-zinc-800 text-white p-20 flex justify-center items-center flex-col gap-8 rounded-3xl">
-          <h1 className="text-5xl font-extrabold">Password Generator</h1>
+      <main className="bg-black w-screen h-screen flex justify-center items-center px-3 py-2">
+        <section className="bg-zinc-800 text-white p-14 flex justify-center items-center flex-col gap-8 rounded-3xl">
+          <h1 className="text-5xl font-extrabold text-yellow-400">
+            Password Generator
+          </h1>
           <div className="flex rounded-xl overflow-hidden">
             <input
               type="text"
               value={password}
               placeholder="Password"
               readOnly
+              ref={passwordReference}
               className="text-black text-2xl font-semibold outline-none cursor-pointer px-5 py-2 w-full"
             />
-            <button className="bg-blue-800 px-5 py-2 font-medium">Copy</button>
+            <button
+              onClick={copyPasswordToClipBoard}
+              className="bg-blue-800 px-5 py-2 font-medium hover:bg-red-500 duration-200"
+            >
+              Copy
+            </button>
           </div>
           <div className="flex flex-col gap-2 items-start">
             <div className="flex justify-center items-center gap-7">
               <input
                 type="range"
                 min={6}
-                maxLength={100}
+                max={20}
+                defaultValue={length}
                 onChange={(event) => {
                   setLength(event.target.value);
                 }}
                 className="cursor-pointer scale-125"
               />
-              <label className="text-orange-500 font-medium text-lg">
-                Length: {length}
+              <label className="text-red-500 font-medium text-lg">
+                Length :- {length}
+              </label>
+            </div>
+            <div className="flex justify-center items-center gap-1">
+              <input
+                type="checkbox"
+                defaultChecked
+                id="lowerCaseCheck"
+                onChange={() => {
+                  setLowerCaseAllowed((prev) => !prev);
+                }}
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                htmlFor="lowerCaseCheck"
+                className="text-orange-500 font-medium text-lg"
+              >
+                Allow LowerCase Alphabets
+              </label>
+            </div>
+            <div className="flex justify-center items-center gap-1">
+              <input
+                type="checkbox"
+                id="upperCaseCheck"
+                onChange={() => {
+                  setUpperCaseAllowed((prev) => !prev);
+                }}
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                htmlFor="upperCaseCheck"
+                className="text-orange-500 font-medium text-lg"
+              >
+                Allow UpperCase Alphabets
               </label>
             </div>
             <div className="flex justify-center items-center gap-1">
@@ -66,7 +133,7 @@ function App() {
                 htmlFor="numCheck"
                 className="text-orange-500 font-medium text-lg"
               >
-                Numbers
+                Allow Numbers or Digits
               </label>
             </div>
             <div className="flex justify-center items-center gap-1">
@@ -82,7 +149,7 @@ function App() {
                 htmlFor="charCheck"
                 className="text-orange-500 font-medium text-lg"
               >
-                Characters
+                Allow Special Characters
               </label>
             </div>
           </div>
