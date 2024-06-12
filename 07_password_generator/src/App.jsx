f49dd3e16008cdeace38f6ff1f18cf26a36dca1f
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-12 11:26:00 +0530

    used useCallback hook

--- 07_password_generator/src/App.jsx
+++ 07_password_generator/src/App.jsx
@@ -1,7 +1,25 @@
+import { useCallback, useState } from "react";
+
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let passwordString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) passwordString += "0123456789";
+    if (characterAllowed) passwordString += "!@#$%^&*-_+=[]{}~`";
+    for (let index = 1; index <= length; index++) {
+      let character = Math.floor(Math.random() * passwordString.length) + 1;
+      password += passwordString.charAt(character);
+    }
+    setPassword(password);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+
   return (
     <>
-      <h1>Ajay</h1>
+      <main className="bg-black w-screen h-screen text-white"></main>
     </>
   );
 }
