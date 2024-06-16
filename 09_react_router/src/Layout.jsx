1ce3bfb40819d05c06d9f6b96d723b5c7161e10d
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-16 12:02:58 +0530

    react router dom routing and parameters capturing done

--- 09_react_router/src/Layout.jsx
+++ 09_react_router/src/Layout.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
+import { Outlet } from "react-router-dom";
 
-function App() {
+function Layout() {
   return (
     <>
-      {/* <Header />
-      <Home />
-      <Footer /> */}
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   );
 }
 
-export default App;
+export default Layout;
