78af1342935ffdd469bd6aa0ec878c473a860487
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-25 15:18:20 +0530

    other work + tailwind css setup

--- 14_mega_appwrite_blog_project/src/App.jsx
+++ 14_mega_appwrite_blog_project/src/App.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, [third]);
+
+  return !loading ? "" : "";
 }
 
 export default App;
