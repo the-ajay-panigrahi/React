e65c19c3e453fc61181e55da9cd4b52fb4af38b5
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-25 16:01:13 +0530

    some more configuration

--- 14_mega_appwrite_blog_project/src/App.jsx
+++ 14_mega_appwrite_blog_project/src/App.jsx
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import "./App.css";
+import { Outlet } from "react-router-dom";
+import { Footer, Header } from "./components";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -19,9 +21,19 @@ function App() {
         }
       })
       .finally(() => setLoading(false));
-  }, [third]);
+  }, []);
 
-  return !loading ? "" : "";
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block ">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
--- 14_mega_appwrite_blog_project/src/store/store.js
+++ 14_mega_appwrite_blog_project/src/store/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: authReducer,
 });
+
+export default store;
