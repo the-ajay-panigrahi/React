4581815b6bcf33b16e4d04b4582fde404a0f947e
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-25 11:29:16 +0530

    file upload service done

--- 14_mega_appwrite_blog_project/src/appwrite/config.js
+++ 14_mega_appwrite_blog_project/src/appwrite/config.js
@@ -90,6 +90,34 @@ export class Service {
       return false;
     }
   }
+
+  //   file upload service
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite service :: uploadFile :: error ", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Appwrite service :: deleteFile :: error ", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+  }
 }
 
 const service = new Service();
